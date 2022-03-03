167b636469776e221f9756650622210d49e67a36
nitzanpap <nitzanpap@gmail.com>
2022-03-03 15:51:02 +0200

    addCrossAtSpot() works, but is called badly

--- app.js
+++ app.js
@@ -1,74 +1,54 @@
 document.addEventListener("DOMContentLoaded", () => {
-    //card options
-    const cardArray = [
-        {
-            name: "fries",
-            img: "images/fries.png",
-        },
-        {
-            name: "cheeseburger",
-            img: "images/cheeseburger.png",
-        },
-    ]
-
-    const board = document.querySelector(".board")
-    const spots = document.querySelectorAll(".spot")
+    const spots = Array.from(document.querySelectorAll(".spot"))
     console.log(spots)
-    let matrix = [
-        ["", "", ""],
-        ["", "", ""],
-        ["", "", ""],
+
+    let board = ["", "", "", "", "", "", "", "", ""]
+    let currentPlayer = X
+    let isGameOver = false
+
+    /*
+     [0] [1] [2]
+     [3] [4] [5]
+     [6] [7] [8]
+    */
+    const winningCombinations = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
     ]
-    console.log(spots[0].children[0].className)
 
-    // Create yout board
-    function createBoard() {
-        for (let i = 0; i < spots.length; i++) {
-            let spot = spots[i].children[0]
+    spots.forEach((spot, index) => {
+        let sign = spot.children[0]
+        sign.addEventListener("click", () => addCrossAtSpot(spot))
+    })
 
-            card.addEventListener("click", flipCard)
-            grid.appendChild(card)
+    function addCrossAtSpot(spot, index) {
+        let div = spot.children[0]
+        console.log(div.className)
+        if (div.className == "empty") {
+            const cross = document.createElement("div")
+            cross.classList.add("cross")
+
+            console.log(cross)
+
+            const diagonal1 = document.createElement("div")
+            diagonal1.classList.add("diagonal")
+            diagonal1.setAttribute("id", "line1")
+
+            const diagonal2 = document.createElement("div")
+            diagonal2.classList.add("diagonal")
+            diagonal2.setAttribute("id", "line2")
+
+            cross.appendChild(diagonal1)
+            cross.appendChild(diagonal2)
+            spot.children[0].appendChild(cross)
+            spot.children[0].remove()
+            spot.appendChild(cross)
         }
     }
-
-    function addCrossAtSpot(spot) {
-        let cross = document.createElement("div")
-    }
-
-    // Check for a match
-    function checkForMatch() {
-        let cards = document.querySelectorAll("img")
-        const optionOneId = cardsChosenId[0]
-        const optionTwoId = cardsChosenId[1]
-        if (cardsChosen[0] === cardsChosen[1]) {
-            cards[optionOneId].setAttribute("src", "images/white.png")
-            cards[optionTwoId].setAttribute("src", "images/white.png")
-            alert("You found a match!")
-            cardsWon.push(cardsChosen)
-        } else {
-            cards[optionOneId].setAttribute("src", "images/blank.png")
-            cards[optionTwoId].setAttribute("src", "images/blank.png")
-            alert("Sorry! try again.")
-        }
-        cardsChosen = []
-        cardsChosenId = []
-        resultDisplay.textContent = cardsWon.length
-        if (cardsWon.length === cardArray.length / 2) {
-            resultDisplay.textContent = "Congratulations! You won the game!"
-            grid.classList.toggle("disabled")
-        }
-    }
-
-    // Flip your card
-    function flipCard() {
-        let cardId = this.getAttribute("data-id")
-        cardsChosen.push(cardArray[cardId].name)
-        cardsChosenId.push(cardId)
-        this.setAttribute("src", cardArray[cardId].img)
-        if (cardsChosen.length === 2) {
-            setTimeout(checkForMatch, 500)
-        }
-    }
-
-    createBoard()
 })
