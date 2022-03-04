8c78d20f0f465b2c789a8d6dbb48254f71104a8c
nitzanpap <nitzanpap@gmail.com>
2022-03-04 03:45:44 +0200

    Fixed #6

--- app.js
+++ app.js
@@ -77,7 +77,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 msg.innerHTML = ""
                 break
             case "Marked Spot":
-                msg.innerHTML = "This spot has already been marked."
+                msg.innerHTML = "This spot is already marked."
                 msg.style.color = "crimson"
                 break
             case "Game Won":
