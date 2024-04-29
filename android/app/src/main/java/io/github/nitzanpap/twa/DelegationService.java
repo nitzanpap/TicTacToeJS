814fa57b620cedbbb488fe71cad689f38c4680f3
nitzanpap <nitzanpap@gmail.com>
2024-04-29 15:32:58 +0300

    android files

---
+++ android/app/src/main/java/io/github/nitzanpap/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package io.github.nitzanpap.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
